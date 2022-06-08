498868bc282cb5299f707a3a0cd1f60664982626
hgoncerz <hubertg489655@gmail.com>
2022-06-08 11:28:17 +0200

    implemented footer and the end

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,49 +1,47 @@
 import React from "react";
-import { Article } from "../../components";
+import Article from "../../components/article/Article";
 import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
-const Blog = () => {
-  return (
-    <div className="gpt3__blog section__padding" id="blog">
-      <div className="gpt3__blog-heading">
-        <h1 className="gradient__text">
-          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-        </h1>
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">
+        A lot is happening, <br /> We are blogging about it.
+      </h1>
+    </div>
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-container_groupA">
+        <Article
+          imgUrl={blog01}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
       </div>
-      <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container-groupA">
-          <Article
-            imgUrl={blog01}
-            date="sep 26, 2022"
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, cupiditate?"
-          />
-        </div>
-        <div className="gpt3__blog-container-groupB">
-          <Article
-            imgUrl={blog02}
-            date="sep 26, 2022"
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, cupiditate?"
-          />
-          <Article
-            imgUrl={blog03}
-            date="sep 26, 2022"
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, cupiditate?"
-          />
-          <Article
-            imgUrl={blog04}
-            date="sep 26, 2022"
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, cupiditate?"
-          />
-          <Article
-            imgUrl={blog05}
-            date="sep 26, 2022"
-            title="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, cupiditate?"
-          />
-        </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article
+          imgUrl={blog02}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog03}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog04}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog05}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Blog;
