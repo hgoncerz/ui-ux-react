f17e8b81ab969068518eec2b4bb66a8d6e466e37
hgoncerz <hubertg489655@gmail.com>
2022-06-08 10:10:10 +0200

    implemented cta component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h3>Lorem ipsum dolor sit amet.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
